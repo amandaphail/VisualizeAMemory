8d2be4d370f2e25c15715cbcc4ee46af8541d3a7
amanda <amandalphail@gmail.com>
2021-03-26 16:53:05 -0400

    import axios with hidden key console.log gets info

--- src/components/MemoriesList.jsx
+++ src/components/MemoriesList.jsx
@@ -1,6 +1,21 @@
 import React from 'react'
-
+import axios from 'axios'
+import {useState, useEffect} from "react"
+import {baseURL, config} from "../services"
 export default function MemoriesList() {
+
+    const [memories,setMemories] = useState([])
+
+    useEffect(()=> { 
+        getMemories()
+    },[])
+
+    async function getMemories(){
+        let response = await axios.get(baseURL, config)
+        console.log (response)
+    }
+    
+
     return (
         <div>
             MemsList
