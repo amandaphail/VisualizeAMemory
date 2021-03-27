5352298c6604ca6e2946f64f7e72ca12d80f3e7b
amanda <amandalphail@gmail.com>
2021-03-26 22:17:20 -0400

    yoinked url from embed link from google using split

--- src/components/Memories.jsx
+++ src/components/Memories.jsx
@@ -2,10 +2,32 @@ import React from 'react'
 
 export default function Memories(props) {
 
-    console.log(props.memory.fields)
+    let mem = props.memory.fields
+
+    let mapInput = props.memory.fields.memoryLink
+    
+    let splitMap = mapInput.split(`"`)
+    let userLink = splitMap[1]
+
+    console.log(mapInput)
+    // console.log(mapInput)
+    // console.log(mem)
+
     return (
         <div>
-            Mems
+            <br/>
+        <div id = "left">
+            <p>
+                {mem.description}
+            </p>
+            <p>{mem.name}</p>
         </div>
+        <br/>
+        <div id ="right">
+            {userLink}
+        </div>
+        <br />
+        </div>
+        
     )
 }
