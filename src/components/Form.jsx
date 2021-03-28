0ab20b791777e6010135a2a541ae967c505509c3
amanda <amandalphail@gmail.com>
2021-03-28 15:18:23 -0400

    set memory state and hooked it up to form values

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,29 +1,44 @@
 import React from 'react'
+import {useState} from "react"
 
 export default function Form() {
+
+let [memory, setMemory]= useState({
+    name: "",
+    description: "",
+    memoryLink: "",
+    location: "",
+    type: "",
+})
+
+function handleChange(event){
+    let {name, value} = event.target
+    setMemory((prevState)=>({...prevState,[name]:value}))
+}
+
     return (
         <div>
             <form>
                 <label htmlFor="name">
                     Name:  
                 </label>
-                <input type ="text" name = "name" id="name" required/>
+                <input type ="text" name = "name" id="name" required onChange={handleChange} value={memory.name}/>
                 <label htmlFor="description">
                     Description: 
                 </label>
-                <input type ="text" name = "description" id="description" required/>
-                <label htmlFor="imageURL">
+                <input type ="text" name = "description" id="description" required onChange={handleChange} value={memory.description}/>
+                <label htmlFor="memoryLink">
                     Google Maps Embed Link:   
                 </label>
-                <input type ="text" name = "imageURL" id="imageURL" required/>
+                <input type ="text" name = "memoryLink" id="memoryLink" required onChange={handleChange} value={memory.memoryLink}/>
                 <label htmlFor="location">
                     Location:  
                 </label>
-                <input type ="text" name = "location" id="location" required/>
+                <input type ="text" name = "location" id="location" required onChange={handleChange} value={memory.location}/>
                 <label htmlFor="type">
                     Type:  
                 </label>
-                <input type ="text" name = "type" id="type" required/>
+                <input type ="text" name = "type" id="type" required onChange={handleChange} value={memory.type}/>
 
             </form>
         </div>
