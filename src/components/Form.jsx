2deff7e5b4c6a5a560e343d6ed5a2b42ae7355e1
amanda <amandalphail@gmail.com>
2021-03-28 15:03:15 -0400

    create form and inputs

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,7 +3,29 @@ import React from 'react'
 export default function Form() {
     return (
         <div>
-            Form
+            <form>
+                <label htmlFor="name">
+                    Name:  
+                </label>
+                <input type ="text" name = "name" id="name" required/>
+                <label htmlFor="description">
+                    Description: 
+                </label>
+                <input type ="text" name = "description" id="description" required/>
+                <label htmlFor="imageURL">
+                    Google Maps Embed Link:   
+                </label>
+                <input type ="text" name = "imageURL" id="imageURL" required/>
+                <label htmlFor="location">
+                    Location:  
+                </label>
+                <input type ="text" name = "location" id="location" required/>
+                <label htmlFor="type">
+                    Type:  
+                </label>
+                <input type ="text" name = "type" id="type" required/>
+
+            </form>
         </div>
     )
 }
