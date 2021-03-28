d8ae3df2ab38221df861344e6128644bcc8f3d6c
amanda <amandalphail@gmail.com>
2021-03-28 18:11:41 -0400

    add more image guides and import it to user guide

--- src/components/Guide.jsx
+++ src/components/Guide.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
-import logo from "../UserGuideImages/dotsEdited.png"
+import step1 from "../UserGuideImages/streetView.png"
+import step2 from "../UserGuideImages/dotsEdited.png"
+import step3 from "../UserGuideImages/embedEdited.png"
+//https://stackoverflow.com/questions/39999367/how-do-i-reference-a-local-image-in-react
 
 export default function Guide() {
 
     return (
         <div>
-           <p>
+           <h1>
                This is how you can share your own memories!
-               </p> 
-            <img src={logo}/>
+               </h1> 
+               <h4 id="warning">Please be advised, you can only create a post on a computer, not on a mobile device</h4>
+            <p>Step 1:</p>
+            <img src={step1}/>
+            <p>Step 2:</p>
+            <img src={step2}/>
+            <p>Step 3:</p>
+            <img src={step3}/>
         </div>
     )
 }
