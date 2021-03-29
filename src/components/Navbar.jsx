9221bf952daf83e45f9da4defee43e82a2d87acb
amanda <amandalphail@gmail.com>
2021-03-29 13:10:37 -0400

    structure changes for styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom"
 
 export default function Navbar() {
     return (
-        <div>
+        <div id="navbar">
             
             <Link to ="/">Home </Link>
             <Link to="/new">Share a Memory </Link>
