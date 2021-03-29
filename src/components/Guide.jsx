1031e18575e3730c4707ad22e08fc6850c577aee
amanda <amandalphail@gmail.com>
2021-03-29 17:30:09 -0400

    guide css

--- src/components/Guide.jsx
+++ src/components/Guide.jsx
@@ -3,14 +3,15 @@ import step1 from "../UserGuideImages/streetView.png"
 import step2 from "../UserGuideImages/dotsEdited.png"
 import step3 from "../UserGuideImages/embedEdited.png"
 //https://stackoverflow.com/questions/39999367/how-do-i-reference-a-local-image-in-react
+import "./guide.css"
 
 export default function Guide() {
 
     return (
         <div>
-           <h1>
+           <h2>
                This is how you can share your own memories!
-               </h1> 
+               </h2> 
                <h4 id="warning">Please be advised, you can only create a post on a computer, not on a mobile device</h4>
             <p>Step 1:</p>
             <img src={step1} alt = "step 1"/>
