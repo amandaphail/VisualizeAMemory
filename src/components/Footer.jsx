250c2f5154ac032634de152ebd74a81bd8b34188
amanda <amandalphail@gmail.com>
2021-03-30 11:40:10 -0400

    footer styled and complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import logo from "./GitHubLogo/GitHub-Mark-64px.png"
+import "./footer.css"
 
 export default function Footer() {
     return (
-        <div>
+        <div id="footer">
             <a href="https://github.com/amandaphail">
   <img src={logo} alt="Link to my GitHub" id="logo" />
 </a>
