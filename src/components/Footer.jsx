6fb83181a1cc1fbeb9506bedda96fbc6c01466e8
amanda <amandalphail@gmail.com>
2021-03-30 11:34:15 -0400

    logo links to my github

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ import logo from "./GitHubLogo/GitHub-Mark-64px.png"
 export default function Footer() {
     return (
         <div>
-            {/* <a href="default.asp"> */}
+            <a href="https://github.com/amandaphail">
   <img src={logo} alt="Link to my GitHub" id="logo" />
-{/* </a> */}
+</a>
         </div>
     )
 }
