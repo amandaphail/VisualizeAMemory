7332d50a4fd36b48ee9d782bc39d4be84a29f688
amanda <amandalphail@gmail.com>
2021-03-30 11:58:25 -0400

    memories component styling

--- src/components/MemoriesList.jsx
+++ src/components/MemoriesList.jsx
@@ -7,10 +7,16 @@ export default function MemoriesList(props) {
 let memories = props.memories
     return (
         <div>
+            <div id="intro">
+                Expand any memory that resonates, and you can see into the world where it happened!
+            </div>
+            <div>
+
             {memories.map((memory, index) =>{
                 // console.log(memory.fields)
                 return <Memories key = {index} memory={memory} id = {memory.id}/>
             })}
+            </div>
         </div>
     )
 }
