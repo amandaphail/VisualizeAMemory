45ec95f3a4b0dfadb7498cc26b6b07351460ff0e
amanda <amandalphail@gmail.com>
2021-03-30 11:28:28 -0400

    imported footer github logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import logo from "./GitHubLogo/GitHub-Mark-64px.png"
 
 export default function Footer() {
     return (
         <div>
-            Footer
+            {/* <a href="default.asp"> */}
+  <img src={logo} alt="Link to my GitHub" id="logo" />
+{/* </a> */}
         </div>
     )
 }
