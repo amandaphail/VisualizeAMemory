aad369bb8264320814be8802bc6e8eaf852c2be3
amanda <amandalphail@gmail.com>
2021-03-30 20:36:19 -0400

    add instructions to guide

--- src/components/Guide.jsx
+++ src/components/Guide.jsx
@@ -8,12 +8,17 @@ import "./guide.css"
 export default function Guide() {
 
     return (
-        <div>
+        <div id="center">
            <h2>
                This is how you can share your own memories!
-               </h2> 
-               <h4 id="warning">Please be advised, you can only create a post on a computer, not on a mobile device</h4>
+            </h2> 
+            <h4 id="warning">
+                Please be advised, you can only create a post on a computer, not on a mobile device
+            </h4>
             <p>Step 1:</p>
+            <p class ="instructions">
+                Go to Google Maps and find the exact spot where your memory took place.  Take the person icon in the bottom right and place them to get the street view.
+            </p>
             <img src={step1} alt = "step 1"/>
             <p>Step 2:</p>
             <img src={step2} alt = "step 2"/>
