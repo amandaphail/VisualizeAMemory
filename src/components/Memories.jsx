44eb8d342b372c4792893857b3887381946e1fd1
amanda <amandalphail@gmail.com>
2021-03-31 14:03:55 -0400

    accoridion mediaquery

--- src/components/Memories.jsx
+++ src/components/Memories.jsx
@@ -51,8 +51,8 @@ export default function Memories(props) {
             <Accordion>
                 <AccordionSummary expandIcon={<ExpandMore />}>
                     <div id = "left">
-                        <p> {mem.description}</p>
-                        <p>{mem.name}</p>
+                        <p class="memory"> {mem.description}</p>
+                        <p class="memory">{mem.name}</p>
                     </div>
                 </AccordionSummary>
                 <AccordionDetails>
